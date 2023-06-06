b7503949aaaafdc9cd63b70120736d9ca3d11cae
Cássio Reis <csreis04@gmail.com>
2023-06-06 19:17:41 -0300

    alterações na janela das filiais

--- gerencia_de_estoque/src/view/JanelaFilial.java
+++ gerencia_de_estoque/src/view/JanelaFilial.java
@@ -3,26 +3,29 @@ import java.awt.Font;
 import javax.swing.*;
 
 public class JanelaFilial {
-	private JButton createFilial = new JButton("Adicionar Filial");
-	private JButton refreshFilial = new JButton("Atualizar Filial");
 	private JLabel texto = new JLabel("Pesquisar filial");
 	private JList<String> listaFiliais = new JList<String>();
+	private JButton botaoCriarFil = new JButton("Adicionar Filial");
+	private JButton botaoVerFil = new JButton("Ver filial");
+	private JButton botaoVerEst = new JButton("Ver estoque");
 	private JFrame janela = new JFrame("Filiais");
 	
 	public JanelaFilial() {
-		texto.setBounds(10, 10, 150, 20);
+		texto.setBounds(20, 10, 150, 20);
 		texto.setFont(new Font("Arial", Font.BOLD, 20));
-		listaFiliais.setBounds(180, 10, 200, 20);
+		listaFiliais.setBounds(20, 50, 200, 200);
 		listaFiliais.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		listaFiliais.setVisibleRowCount(10);
-		createFilial.setBounds(140, 50, 120, 30);
-		refreshFilial.setBounds(140, 300, 120, 30);
+		botaoCriarFil.setBounds(60, 270, 120, 30);
+		botaoVerFil.setBounds(250, 110, 120, 30);
+		botaoVerEst.setBounds(250, 170, 120, 30);
 		
 		janela.setLayout(null);
 		janela.add(texto);
 		janela.add(listaFiliais);
-		janela.add(createFilial);
-		janela.add(refreshFilial);
+		janela.add(botaoCriarFil);
+		janela.add(botaoVerEst);
+		janela.add(botaoVerFil);
 		
 		janela.setSize(400, 400);
 		janela.setVisible(true);
