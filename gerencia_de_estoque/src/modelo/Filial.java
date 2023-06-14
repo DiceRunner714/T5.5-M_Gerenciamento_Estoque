b79073532b60ced569fb2dc8128a313c916aac5a
Hunter104 <000plagueinc@gmail.com>
2023-06-14 08:31:29 -0300

    Adicionado javadoc parcial da classe Filial

--- gerencia_de_estoque/src/modelo/Filial.java
+++ gerencia_de_estoque/src/modelo/Filial.java
@@ -4,12 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+/**Classe Filial representa uma filial associada a uma empresa
+ * @author André Emanuel Bispo da Silva
+ * @since 2023
+ * @version 1.0
+ * @see Item
+ * @see Empresa
+ */
 public class Filial implements LeitordeEstoque {
     private String nome;
     private String local;
     private int id;
     private List<Item> estoque;
 
+    /**Cria uma filial com o nome, local geográfico e número de identificação especificados,
+     * criando um estoque vazio para a tal
+     * @param nome nome da filial
+     * @param local localização geográfica da filial
+     * @param id identificação da filial
+     */
     public Filial(String nome, String local, int id) {
         this.nome = nome;
         this.local = local;
@@ -17,20 +30,48 @@ public class Filial implements LeitordeEstoque {
         estoque = new ArrayList<>();
     }
 
+    /**Retora uma string formatada com as características básicas da filial,
+     * destinada ao uso na linha de comando
+     * @return uma string com todas as características básicas da filial
+     */
+    public String listarCaracteristicasBasicacs() {
+        return String.format("""
+                ---Filial---
+                nome: %s
+                local: %s
+                id: %d
+                """, nome, local, id);
+    }
+
     // -- CRUD DE ITEM --
+
+    /**Adiciona o item criado ao estoque da filial
+     * @param i item a ser adicionado
+     */
     public void adicionarItem(Item i) {
         estoque.add(i);
     }
 
+    /**Remove o item com o número de identificação especificado do estoque da filial
+     * @param itemId número de identificação do item a ser removido
+     */
     public void removerItem(int itemId) {
         estoque.removeIf(item -> item.getId() == id);
     }
 
+    /**Remove o item com as mesmas características do item especificado do estoque da filial
+     * @param itemEscolhido item a ser removido do estqoue da filial
+     */
     public void removerItem(Item itemEscolhido) {
         estoque.removeIf(item -> item.equals(itemEscolhido));
     }
 
     // -- LEITURA DE ITEM --
+
+    /**Busca um item no estoque com o nome exato especificado
+     * @param nome nome do item a buscar
+     * @return primeiro item com esse mesmo nome, caso não exista retorna null
+     */
     @Deprecated
     public Item buscarItem(String nome) {
         return estoque
@@ -40,11 +81,21 @@ public class Filial implements LeitordeEstoque {
                 .orElse(null);
     }
 
+    /**Busca um item no estoque com o número de indentificação especificado
+     * @param id número de identificação do item a ser buscado
+     * @return o primeiro item com esse número de identificação
+     * @throws java.util.NoSuchElementException quando nenhum item é encontrado
+     */
     @Override
     public Item buscarItem(int id) {
         return estoque.stream().filter(item -> item.getId() == id).findFirst().orElseThrow();
     }
 
+    /**Busca todos os itens do estoque com o nome que contém uma certa string
+     * @param nomeParcial string a ser comparada com o nome dos itens
+     * @param caseSensitive sensibilidade a letras maiúsculas e minúsculas
+     * @return todos os itens com o nome que contém o nome parcial
+     */
     @Override
     public List<Item> buscarItensParcial(String nomeParcial, boolean caseSensitive) {
         if (caseSensitive) return
@@ -55,6 +106,11 @@ public class Filial implements LeitordeEstoque {
                         .toList();
     }
 
+    /**Busca todos os itens do estoque com o mesmo nome especificado
+     * @param nome nome dos itens a serem pesquisados
+     * @param caseSensitive sensibilidade a letras maiúsculas e minúsculas
+     * @return todos os itens com o mesmo nome especificado
+     */
     @Override
     public List<Item> buscarItens(String nome, boolean caseSensitive) {
         if (caseSensitive) return
@@ -63,32 +119,39 @@ public class Filial implements LeitordeEstoque {
                 estoque.stream().filter(item -> item.getNome().equalsIgnoreCase(nome)).toList();
     }
 
-    public String listarCaracteristicasBasicacs() {
-        return String.format("""
-                ---Filial---
-                nome: %s
-                local: %s
-                id: %d
-                """, nome, local, id);
-    }
-
+    /**Override da classe Object, representa a filial com o número de identificação e seu nome
+     * @return uma string com o id da filial e seu nome
+     */
     @Override
     public String toString() {
         return String.format("%d_%s", id, nome);
     }
 
+    /**Retorna o estoque da filial
+     * @return uma lista com todos os itens da filial
+     */
     public List<Item> getEstoque() {
         return estoque;
     }
 
+    /**Substitui o estoque da filial
+     * @param estoque lista de itens a ser utilizada na substituição
+     */
     public void setEstoque(List<Item> estoque) {
         this.estoque = estoque;
     }
 
+    /**Retorna os itens sem quantidade no estoque
+     * @return uma lista com todos os itens com quantidade 0 no estoque
+     */
     public List<Item> getEstoqueVazio() {
         return estoque.stream().filter(item -> item.getQuantidade() == 0).toList();
     }
 
+    /**Override da classe Object
+     * @param o
+     * @return
+     */
     @Override
     public boolean equals(Object o) {
         if (o == this) {
@@ -102,6 +165,11 @@ public class Filial implements LeitordeEstoque {
                 this.id == outraFilial.getId();
     }
 
+    /**Busca todos os itens do estoque com um nome contendo a string especificada
+     * @param nomeParcial string a compara com os nomes dos itens
+     * @param caseSensitive sensibilidade a letras maiúsculas e minúsculas
+     * @return todos os itens com o nome contendo a string especificada
+     */
     public List<Item> buscaParcial(String nomeParcial, boolean caseSensitive) {
         if (caseSensitive)  {
             return estoque.stream()
@@ -115,10 +183,16 @@ public class Filial implements LeitordeEstoque {
 
     }
 
+    /**
+     * @return nome da filial
+     */
     public String getNome() {
         return nome;
     }
 
+    /**
+     * @param nome nome da filial
+     */
     public void setNome(String nome) {
         this.nome = nome;
     }
@@ -127,14 +201,23 @@ public class Filial implements LeitordeEstoque {
         return local;
     }
 
+    /**
+     * @param local locallização geográfica da filial
+     */
     public void setLocal(String local) {
         this.local = local;
     }
 
+    /**
+     * @return número de identificação da filial
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * @param id número de identificação da filial
+     */
     public void setId(int id) {
         this.id = id;
     }
