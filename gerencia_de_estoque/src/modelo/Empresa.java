9de53f43ef795bc1003fc7419af8fc35bfef084e
Hunter104 <pudimdeleite2004@outlook.com>
2023-06-22 11:29:37 -0300

    Formatação

--- gerencia_de_estoque/src/modelo/Empresa.java
+++ gerencia_de_estoque/src/modelo/Empresa.java
@@ -3,18 +3,22 @@ package modelo;
 import java.util.ArrayList;
 import java.util.List;
 
-/**Classe Empresa representa uma empresa que agrega filiais com números de indentificação distintas
+/**
+ * Classe Empresa representa uma empresa que agrega filiais com números de indentificação distintas
+ *
  * @author André Emanuel Bipo da Silva
- * @since 2023
  * @version 1.0
  * @see Filial
+ * @since 2023
  */
 public class Empresa implements LeitordeEstoque {
 
     private String nome;
     private List<Filial> filiais;
 
-    /**Cria uma empresa com o nome especificado
+    /**
+     * Cria uma empresa com o nome especificado
+     *
      * @param nome nome da empresa
      */
     public Empresa(String nome) {
@@ -35,9 +39,10 @@ public class Empresa implements LeitordeEstoque {
 
     /**
      * Retorna a primeira filial da lista de filiais com o id escolhido, caso não exista joga uma exceção
-     * @throws java.util.NoSuchElementException caso não exista uma filial com esse Id
+     *
      * @param id id da filial a ser pesquisada
      * @return primeira filial com o mesmo id presente na lista de filiais
+     * @throws java.util.NoSuchElementException caso não exista uma filial com esse Id
      */
     public Filial lerFilial(int id) {
         return filiais.stream().filter(filial -> filial.getId() == id).findFirst().orElseThrow();
@@ -45,9 +50,10 @@ public class Empresa implements LeitordeEstoque {
 
     /**
      * Cria uma filial com as características especificadas e insere na lista de filiais
-     * @param nome nome da filial a ser adicionada
+     *
+     * @param nome  nome da filial a ser adicionada
      * @param local local geográfico da filial a ser adicionada
-     * @param id número de indentificação da filial a ser adicionada
+     * @param id    número de indentificação da filial a ser adicionada
      */
     public void adicionarFilial(String nome, String local, int id) {
         filiais.add(new Filial(nome, local, id));
@@ -55,6 +61,7 @@ public class Empresa implements LeitordeEstoque {
 
     /**
      * Adiciona a filial criada na lista de filiais
+     *
      * @param f filial a ser adicionada na lista
      */
     public void adicionarFilial(Filial f) {
@@ -63,6 +70,7 @@ public class Empresa implements LeitordeEstoque {
 
     /**
      * Remove a filial com o número de indentificação especificado da lista de filiais
+     *
      * @param id número de indenfiticação da filial a ser removida
      */
     public void removerFilial(int id) {
@@ -71,6 +79,7 @@ public class Empresa implements LeitordeEstoque {
 
     /**
      * Remove a filial que é igual a filial especificada da lista de filiais
+     *
      * @param f filial a ser removida da lista
      */
     public void removerFilial(Filial f) {
@@ -79,10 +88,11 @@ public class Empresa implements LeitordeEstoque {
 
     /**
      * Modifica os campos da filial com o número de indentificação especificado na lista
-     * @param nome novo nome da filial
-     * @param local novo local geográfico da filial
+     *
+     * @param nome   novo nome da filial
+     * @param local  novo local geográfico da filial
      * @param novoId novo id da filial
-     * @param id id antigo da filial
+     * @param id     id antigo da filial
      */
     public void atualizarFilial(String nome, String local, int novoId, int id) {
         Filial filial = lerFilial(id);
@@ -94,7 +104,9 @@ public class Empresa implements LeitordeEstoque {
 
     // -- LEITURA DE ESTOQUE GERAL --
 
-    /**Retorna uma lista com todos os itens dos estoques de cada filial presente na empresa
+    /**
+     * Retorna uma lista com todos os itens dos estoques de cada filial presente na empresa
+     *
      * @return uma lista com todos os itens de todas as filiais
      */
     @Override
@@ -104,17 +116,10 @@ public class Empresa implements LeitordeEstoque {
         return todosItens;
     }
 
-    /**Busca no estoque inteiro o primeiro item com nome definido, sensível a maiúsculas e minúsculas
-     * @param nome nome a ser pesquisado
-     * @return primeiro item com esse mesmo nome
-     */
-    @Deprecated
-    public Item buscarItem(String nome) {
-        return getEstoque().stream().filter(item -> item.getNome().equals(nome)).findFirst().orElse(null);
-    }
-
-    /**Busca no estoque inteiro o primeiro item com o id definido, joga uma exceção caso não seja encontrado
+    /**
+     * Busca no estoque inteiro o primeiro item com o id definido, joga uma exceção caso não seja encontrado
      * um item
+     *
      * @param id identificação do item a ser pesquisado
      * @return primeiro item com o id escolhido
      * @throws java.util.NoSuchElementException caso não exista uma filial com esse Id
@@ -124,9 +129,10 @@ public class Empresa implements LeitordeEstoque {
         return getEstoque().stream().filter(item -> item.getNome().equals(nome)).findFirst().orElseThrow();
     }
 
-
-    /**Busca no estoque inteiro todos os itens com nomes que contenham a string definida
-     * @param nomeParcial string parcial a ser usada na pesquisa
+    /**
+     * Busca no estoque inteiro todos os itens com nomes que contenham a string definida
+     *
+     * @param nomeParcial   string parcial a ser usada na pesquisa
      * @param caseSensitive sensibilidade a letras maiúsculas ou minúsculas
      * @return lista de itens com nomes que contém a string definida
      */
@@ -139,8 +145,10 @@ public class Empresa implements LeitordeEstoque {
                         .filter(item -> item.getNome().toLowerCase().contains(nomeParcial.toLowerCase())).toList();
     }
 
-    /**Busca no estoque inteiro todos os itens com nomes iguais à string definida
-     * @param nome nome do item a ser pesquisado
+    /**
+     * Busca no estoque inteiro todos os itens com nomes iguais à string definida
+     *
+     * @param nome          nome do item a ser pesquisado
      * @param caseSensitive sensibilidade a letras maiúsculas ou minúsculas
      * @return lista de itens com nomes iguais ao definido
      */
@@ -153,33 +161,28 @@ public class Empresa implements LeitordeEstoque {
     }
 
     /**
-     * Retorna o nome da empresa
-     * @return nome da empresa
+     * Busca no estoque inteiro o primeiro item com nome definido, sensível a maiúsculas e minúsculas
+     *
+     * @param nome nome a ser pesquisado
+     * @return primeiro item com esse mesmo nome
      */
+    @Deprecated
+    public Item buscarItem(String nome) {
+        return getEstoque().stream().filter(item -> item.getNome().equals(nome)).findFirst().orElse(null);
+    }
+
     public String getNome() {
         return nome;
     }
 
-    /**
-     * Altera nome da empresa
-     * @param nome nome novo
-     */
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-    /**
-     * Retrona lista de todas as filiais
-     * @return lista de todas as filiais
-     */
     public List<Filial> getFiliais() {
         return filiais;
     }
 
-    /**
-     * substitui a lista de filiais
-     * @param filiais lista nova de filiais
-     */
     public void setFiliais(ArrayList<Filial> filiais) {
         this.filiais = filiais;
     }
