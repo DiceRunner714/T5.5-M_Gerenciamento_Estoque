d119c0c14ad55bb54b76fbf87aa49d586a9e31b0
Cássio Reis <csreis04@gmail.com>
2023-06-24 12:00:56 -0300

    Adicionado Javadoc da PesquisaView

--- gerencia_de_estoque/src/view/PesquisaView.java
+++ gerencia_de_estoque/src/view/PesquisaView.java
@@ -7,14 +7,42 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.NoSuchElementException;
 
+/**
+ * Classe abstrata PesquisaView possui componentes que realizam as operações
+ * de adicionar, visualizar e modificar algum elemento
+ * @author André Emanuel Bipo da Silva
+ * @version 1.0
+ * @see PesquisaViewFilial
+ * @see PesquisaViewEstoque
+ * @since 2023
+ */
 public abstract class PesquisaView {
     protected ControleEmpresa controleEmpresa;
     protected JButton botaoVerDetalhes;
     protected JButton botaoAdicionar;
+    /**
+     * Atualiza elementos na interface gráfica
+     */
     abstract void refresh();
+    /**
+     * Gera mensagem de erro quando o usuário não seleciona um elemento
+     */
     abstract protected void mensagemErroEscolhaVazia();
+    /**
+     * Cria janela para adição de novo elemento
+     */
     abstract protected void adicionarElemento();
+    /**
+     * Cria janela para visualizar ou modificar um elemento existente
+     */
     abstract protected void visualizarElemento();
+    
+    /**
+     * Controla os eventos de adicionar e visualizar elemento
+	 * @author André Emanuel Bipo da Silva
+	 * @version 1.0
+	 * @since 2023
+     */
     protected class ManipularElementoListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             Object src = e.getSource();
