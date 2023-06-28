c6d24bfb4f05247c720f9ab3f5d3c8cb83c76002
Hunter104 <000plagueinc@gmail.com>
2023-06-28 13:31:08 -0300

    adicionar javadoc LeitorEstoque

--- gerencia_de_estoque/src/controle/LeitorEstoque.java
+++ gerencia_de_estoque/src/controle/LeitorEstoque.java
@@ -4,6 +4,12 @@ import modelo.Item;
 
 import java.util.List;
 
+/**
+ * Interface LeitorEstoque define métodos de leitura de um estoque qualquer
+ *  * @author André Emanuel Bipo da Silva
+ *  * @version 1.0
+ *  * @since 2023
+ */
 public interface LeitorEstoque {
     List<Item> buscarItens(String nome);
 
