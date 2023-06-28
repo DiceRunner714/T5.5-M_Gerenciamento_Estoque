eafee46ba092ff211ba42a8dd2ee8cf44c327c5a
Hunter104 <000plagueinc@gmail.com>
2023-06-28 13:31:14 -0300

    adicionar NivelRestricaoInadequadoException

--- gerencia_de_estoque/src/modelo/NivelRestricaoInadequadoException.java
+++ gerencia_de_estoque/src/modelo/NivelRestricaoInadequadoException.java
@@ -1,5 +1,14 @@
 package modelo;
 
+/**
+ * Classe que representa um erro ao operar sobre o nível de restrição
+ * de um item sob condições inadequadas
+ * @author André Emanuel Bipo da Silva
+ * @version 1.0
+ * @see Empresa
+ * @see view.EmpresaView
+ * @since 2023
+ */
 public class NivelRestricaoInadequadoException extends Exception {
     public NivelRestricaoInadequadoException(String mensagemDeErro) {
         super(mensagemDeErro);
