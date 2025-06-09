d7ee983eef124a8dc3ff4cfd3bb738284ad29cce
DiceRunner714 <eduardo.sandes6@gmail.com>
2025-06-09 10:23:32 -0300

    feat: cria construtor na classe item

--- estoque/app/model/item.py
+++ estoque/app/model/item.py
@@ -1,4 +1,10 @@
 from abc import ABC, abstractmethod
 
 class Item(ABC):
-    pass
+    def __init__(self, nome: str, categoria: str, quantidade: int, id: int, valor: float, restrito: bool = False):
+        self._nome = nome
+        self._categoria = categoria,
+        self._quantidade = quantidade
+        self._id = id
+        self._valor = valor
+        self._restrito = restrito
