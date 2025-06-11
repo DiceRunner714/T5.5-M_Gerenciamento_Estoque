f0fbaae3fac18cfb3811b5b773e32334ab9e7910
DiceRunner714 <eduardo.sandes6@gmail.com>
2025-06-11 13:57:51 -0300

    feat: adciona construtor de filial

--- estoque/app/model/filial.py
+++ estoque/app/model/filial.py
@@ -1,2 +1,8 @@
+from model.item import Item
+
 class Filial:
-    pass
+    def __init__(self, nome: str, local: str, id: int):
+        self._nome: str = nome
+        self._local: str = local
+        self._id: int = id
+        self._itens: list[Item] = []
