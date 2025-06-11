301e3a7a954bb34536310f5114e2752807945d1c
DiceRunner714 <eduardo.sandes6@gmail.com>
2025-06-11 14:00:02 -0300

    feat: adiciona metodo adicionar filial na classe empresa

--- estoque/app/model/empresa.py
+++ estoque/app/model/empresa.py
@@ -4,3 +4,7 @@ class Empresa:
     def __init__(self, nome: str):
         self._nome: str = nome
         self._filiais: list[Filial] = []
+
+    def adicionar_filial(self, nome: str, local: str, id: int) -> None:
+        nova_filial = Filial(nome, local, id)
+        self._filiais.append(nova_filial)
