0bd62067605c25cc1d0a1888b22f83a07fd2ad9b
DiceRunner714 <eduardo.sandes6@gmail.com>
2025-06-11 15:03:47 -0300

    feat: finaliza controladora de empresa

--- estoque/app/controller/empresa_controller.py
+++ estoque/app/controller/empresa_controller.py
@@ -2,3 +2,22 @@ from model.empresa import Empresa
 
 empresas: dict[int, Empresa] = {}
 id_counter = 1
+
+def criar_empresa(nome: str) -> Empresa:
+    global id_counter
+    nova = Empresa(nome)
+    empresas[id_counter] = nova
+    id_counter += 1
+    return nova
+
+def listar_empresas() -> list[tuple[int, Empresa]]:
+    return list(empresas.items())
+
+def buscar_empresa_por_id(id: int) -> Empresa | None:
+    return empresas.get(id)
+
+def remover_empresa(id: int) -> bool:
+    if id in empresas:
+        del empresas[id]
+        return True
+    return False
