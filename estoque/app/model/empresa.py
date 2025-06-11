87fd7b2a77f458b1cd5206f9d631decee76c3f4e
DiceRunner714 <eduardo.sandes6@gmail.com>
2025-06-11 14:06:49 -0300

    feat: adiciona metodo buscar filiar na classe empresa

--- estoque/app/model/empresa.py
+++ estoque/app/model/empresa.py
@@ -8,3 +8,10 @@ class Empresa:
     def adicionar_filial(self, nome: str, local: str, id: int) -> None:
         nova_filial = Filial(nome, local, id)
         self._filiais.append(nova_filial)
+
+    
+    def buscar_filial(self, id: int) -> Filial | None:
+        for filial in self._filiais:
+            if filial.id == id:
+                return filial
+        return None
