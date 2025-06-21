d2e37788bc0346f7f8131a1303821d747afbe97b
DiceRunner714 <eduardo.sandes6@gmail.com>
2025-06-21 13:53:31 -0300

    feat(test): add teste busca empresa por id

--- estoque/app/test/test_empresa_api.py
+++ estoque/app/test/test_empresa_api.py
@@ -22,6 +22,16 @@ def test_listar_empresas():
     assert isinstance(data, list)
     assert len(data) > 0
 
+def test_buscar_empresa_por_id():
+    post = client.post("/empresa/", json={"nome": "Empresa Consulta"})
+    id_empresa = post.json()["id"]
+
+    response = client.get(f"/empresa/{id_empresa}")
+    assert response.status_code == 200
+    data = response.json()
+    assert data["id"] == id_empresa
+    assert data["nome"] == "Empresa Consulta"
+
 def test_deletar_empresa():
     post = client.post("/empresa/", json={"nome": "Empresa Delete"})
     id_empresa = post.json()["id"]
