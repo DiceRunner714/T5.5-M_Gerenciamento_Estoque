1b4fb1c5c0369f747c24d5f3318afae483cde733
DiceRunner714 <eduardo.sandes6@gmail.com>
2025-06-23 18:22:29 -0300

    feat: add filial orm

--- estoque/app/model/empresa_orm.py
+++ estoque/app/model/empresa_orm.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from config.database import Base
 
 class EmpresaORM(Base):
@@ -6,3 +7,4 @@ class EmpresaORM(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     nome = Column(String(100), nullable=False)
+    filiais = relationship("FilialORM", back_populates="empresa", cascade="all, delete")
---
+++ estoque/app/model/filial_orm.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from config.database import Base
+
+class FilialORM(Base):
+    __tablename__ = "filial"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String, nullable=False)
+    empresa_id = Column(Integer, ForeignKey("empresa.id"), nullable=False)
+
+    empresa = relationship("EmpresaORM", back_populates="filiais")
