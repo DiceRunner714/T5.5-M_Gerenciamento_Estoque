a34a6adcebf494d9a184aa3d27a2d9522401a788
DiceRunner714 <eduardo.sandes6@gmail.com>
2025-06-24 21:14:55 -0300

    feat: add produto quimico schema

---
+++ estoque/app/schema/produto_quimico_schema.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+
+class ProdutoQuimicoCreate(BaseModel):
+    nome: str
+    categoria: str
+    valor: float
+    quantidade: int
+    toxicidade: int
+    filial_id: int
+
+class ProdutoQuimicoResponse(BaseModel):
+    id: int
+    nome: str
+    categoria: str
+    valor: float
+    quantidade: int
+    toxicidade: int
+    restrito: bool
+    filial_id: int
+
+    class Config:
+        from_attributes = True
