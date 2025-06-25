07c57fc414d98524243f666d18b3ff3c3498170f
DiceRunner714 <eduardo.sandes6@gmail.com>
2025-06-25 10:35:29 -0300

    feat(refactor): add sugestoes do linter

--- estoque/app/main.py
+++ estoque/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from config.database import Base, engine
 from view import empresa_routes
 from view import filial_routes
 from view import produto_quimico_routes
@@ -11,8 +12,6 @@ app.include_router(produto_quimico_routes.router)
 app.include_router(filial_routes.router)
 app.include_router(empresa_routes.router)
 
-from config.database import Base, engine
-from model.empresa_orm import EmpresaORM
 
 @app.on_event("startup")
 def on_startup():
