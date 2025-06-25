4e49be9f0192e826469f04e6d1f254d55266a104
DiceRunner714 <eduardo.sandes6@gmail.com>
2025-06-25 09:58:39 -0300

    feat: add rota de farmaceutico

---
+++ estoque/app/view/farmaceutico_routes.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter, HTTPException
+from schema.farmaceutico_schema import FarmaceuticoCreate, FarmaceuticoResponse
+from controller import farmaceutico_controller
+
+router = APIRouter(prefix="/farmaceutico", tags=["Farmaceutico"])
+
+@router.post("/", response_model=FarmaceuticoResponse)
+def criar(dados: FarmaceuticoCreate):
+    novo = farmaceutico_controller.criar_farmaceutico(dados)
+    return novo
+
+@router.get("/", response_model=list[FarmaceuticoResponse])
+def listar():
+    return farmaceutico_controller.listar_farmaceuticos()
+
+@router.get("/{id}", response_model=FarmaceuticoResponse)
+def buscar(id: int):
+    f = farmaceutico_controller.buscar_farmaceutico_por_id(id)
+    if f:
+        return f
+    raise HTTPException(status_code=404, detail="Farmacêutico não encontrado")
+
+@router.delete("/{id}")
+def deletar(id: int):
+    if farmaceutico_controller.remover_farmaceutico(id):
+        return {"mensagem": "Farmacêutico removido com sucesso"}
+    raise HTTPException(status_code=404, detail="Farmacêutico não encontrado")
