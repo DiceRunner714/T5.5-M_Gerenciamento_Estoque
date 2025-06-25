5f7fd3d67d10825ce3a01dec1bacf3ed1439e3c3
DiceRunner714 <eduardo.sandes6@gmail.com>
2025-06-25 10:23:01 -0300

    feat(refactor): quebra 2 linhas entre funcoes

--- estoque/app/controller/empresa_controller.py
+++ estoque/app/controller/empresa_controller.py
@@ -2,12 +2,14 @@ from sqlalchemy.orm import Session
 from config.database import SessionLocal
 from model.empresa_orm import EmpresaORM
 
+
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
 
 def criar_empresa(nome: str) -> EmpresaORM:
     db: Session = next(get_db())
@@ -17,13 +19,16 @@ def criar_empresa(nome: str) -> EmpresaORM:
     db.refresh(nova)
     return nova
 
+
 def listar_empresas() -> list[EmpresaORM]:
     db: Session = next(get_db())
     return db.query(EmpresaORM).all()
 
+
 def buscar_empresa_por_id(id: int) -> EmpresaORM | None:
     db: Session = next(get_db())
     return db.query(EmpresaORM).filter(EmpresaORM.id == id).first()
+
 
 def remover_empresa(id: int) -> bool:
     db: Session = next(get_db())
