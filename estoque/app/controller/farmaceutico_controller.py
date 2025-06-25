ebbb238d5457f6c4ca9320e5cf2ec1e7b0c75585
DiceRunner714 <eduardo.sandes6@gmail.com>
2025-06-25 09:58:17 -0300

    feat: add controladora de farmaceutico

---
+++ estoque/app/controller/farmaceutico_controller.py
@@ -0,0 +1,32 @@
+from sqlalchemy.orm import Session
+from config.database import SessionLocal
+from model.farmaceutico_orm import FarmaceuticoORM
+
+def criar_farmaceutico(dados) -> FarmaceuticoORM:
+    db: Session = SessionLocal()
+    novo = FarmaceuticoORM(
+        nome=dados.nome,
+        crf=dados.crf,
+        filial_id=dados.filial_id
+    )
+    db.add(novo)
+    db.commit()
+    db.refresh(novo)
+    return novo
+
+def listar_farmaceuticos() -> list[FarmaceuticoORM]:
+    db: Session = SessionLocal()
+    return db.query(FarmaceuticoORM).all()
+
+def buscar_farmaceutico_por_id(id: int) -> FarmaceuticoORM | None:
+    db: Session = SessionLocal()
+    return db.query(FarmaceuticoORM).filter(FarmaceuticoORM.id == id).first()
+
+def remover_farmaceutico(id: int) -> bool:
+    db: Session = SessionLocal()
+    farmaceutico = buscar_farmaceutico_por_id(id)
+    if farmaceutico:
+        db.delete(farmaceutico)
+        db.commit()
+        return True
+    return False
