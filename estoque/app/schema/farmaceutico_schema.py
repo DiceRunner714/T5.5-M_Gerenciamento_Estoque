1cc4c78450789ec26c62d8082fd83240f6539825
DiceRunner714 <eduardo.sandes6@gmail.com>
2025-06-25 09:57:40 -0300

    feat: add schema de farmaceutico

---
+++ estoque/app/schema/farmaceutico_schema.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+class FarmaceuticoCreate(BaseModel):
+    nome: str
+    crf: str
+    filial_id: int
+
+class FarmaceuticoResponse(FarmaceuticoCreate):
+    id: int
+    ativo: bool
+
+    class Config:
+        from_attributes = True
