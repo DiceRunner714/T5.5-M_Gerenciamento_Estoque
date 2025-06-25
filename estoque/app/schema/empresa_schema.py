a34127e7994d549002320e827450a28e03a0aaf9
DiceRunner714 <eduardo.sandes6@gmail.com>
2025-06-25 10:45:08 -0300

    feat(refactor): refatora schema de empresa

--- estoque/app/schema/empresa_schema.py
+++ estoque/app/schema/empresa_schema.py
@@ -1,11 +1,13 @@
 from pydantic import BaseModel
-from typing import Optional
+
 
 class EmpresaBase(BaseModel):
     nome: str
 
+
 class EmpresaCreate(EmpresaBase):
     pass
+
 
 class EmpresaResponse(EmpresaBase):
     id: int
